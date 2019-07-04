00bc00b4a009e833050378dff4ddc476d0d12948
Zero-Day78 <41844052+Zero-Day78@users.noreply.github.com>
2019-07-04 18:54:05 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -73,7 +73,7 @@ bot.login(process.env.TOKEN);
 bot.on("ready", async () => {
 console.log(`${bot.user.username} Bot Ready`);
 
-bot.user.setActivity("v3.1 -help", {type: "STREAMING", url: "https://www.twitch.tv/Take-Two"});
+bot.user.setActivity("v3.1.1 -help", {type: "STREAMING", url: "https://www.twitch.tv/Take-Two"});
 });
 
 
